34515459d6759b165e9fe849cda801808e2ac258
ttahm3d <ttahm3d@gmail.com>
2020-12-20 11:56:34 +0530

    refactor: changed program to use options

--- index.js
+++ index.js
@@ -1,109 +1,100 @@
-var readlineSync = require('readline-sync')
-var chalk = require('chalk')
+var readlineSync = require("readline-sync");
+var chalk = require("chalk");
 
-
-console.log("*********THE TOUGHEST GAME OF THRONES QUIZ*********")
-console.log()
+console.log("*********THE TOUGHEST GAME OF THRONES QUIZ*********");
+console.log();
 var userName = readlineSync.question(chalk.yellow.bold("What is your name? "));
-console.log(chalk.hex('#8e44ad').bold("Welcome " + userName + " !!\n"))
-
-
+console.log(chalk.hex("#8e44ad").bold("Welcome " + userName + " !!\n"));
 
 var questions = [
   {
-    question: {
-      question: "What were the exact words told by Ned Stark to Bran when he cold-bloodedly beheaded a deserter in the pilot episode? ",
-      options:[
-      "Our way is the old way",
-      "All men must die",
-      "Fear cuts deeper than a sword",
-      "It was my duty"
-      ]
+    question:
+      "1. What were the exact words told by Ned Stark to Bran when he cold-bloodedly beheaded a deserter in the pilot episode? ",
+    options: {
+      A: "Our way is the old way",
+      B: "All men must die",
+      C: "Fear cuts deeper than a sword",
+      D: "It was my duty",
     },
-    answer: "Our way is the old way"
+    answer: "A",
   },
   {
-    question: {
-      question: "Where did Rhaegar Targaryen hid Lyanna Starka after abducting her? ",
-      options: [
-      "Dorne",
-      "Tyrosh",
-      "Volantis",
-      "Pentos"
-    ]
+    question:
+      "2. Where did Rhaegar Targaryen hid Lyanna Starka after abducting her? ",
+    options: {
+      A: "Volantis",
+      B: "Tyrosh",
+      C: "Dorne",
+      D: "Pentos",
     },
-    answer: "Dorne"
+    answer: "C",
   },
   {
-    question: {
-      question: "Where Daenerys went to bring back her dragons? ",
-      options: [
-        "Meereen",
-        "The House of Undying",
-        "Dragonstone",
-        "Qohor"
-      ]
+    question: "3. Where Daenerys went to bring back her dragons? ",
+    options: {
+      A: "Meereen",
+      B: "The House of Undying",
+      C: "Dragonstone",
+      D: "Qohor",
     },
-    answer:"The House of Undying"
+    answer: "B",
   },
   {
-    question: {
-      question: "What was Hodor's real name? ",
-      options: [
-        "Wylis",
-        "Walder",
-        "Waif",
-        "Wess"
-      ]
+    question: "4. What was Hodor's real name? ",
+    options: {
+      A: "Wess",
+      B: "Walder",
+      C: "Waif",
+      D: "Wylis",
     },
-    answer: "Wylis"
+    answer: "D",
   },
   {
-    question: {
-      question:"Who was the head of Brotherhood Without Banners? ",
-     options: [
-        "Beric",
-        "Thoros",
-        "Riddle",
-        "Steve"
-      ]
+    question: '5. Who was the head of "Brotherhood Without Banners?" ',
+    options: {
+      A: "Beric",
+      B: "Thoros",
+      C: "Riddle",
+      D: "Steve",
     },
-    answer: "Beric"
-  }
-]
-    
+    answer: "A",
+  },
+];
+
 var score = 0;
 
-function checkTheAnswer(question, answer){
-  let que = question.question;
-  let opt = [...question.options]
-  console.log("------------------------------------")
-  console.log(chalk.hex('#eaff03')(que))
-  for (let i = 0; i < opt.length; i++) {
-    console.log(chalk.hex('#dced6d')(opt[i]))
-  }
-  let userAnswer = readlineSync.question("Enter your answer: ")
-  console.log("Your answer is: " + userAnswer)
-  console.log()
+function checkTheAnswer(question, options, answer) {
+  let que = question;
+  let opt = options;
+  console.log(chalk.bgHex("#0f64cc").inverse(que));
+  console.log();
+  console.log(chalk.hex("#dced6d")("A: " + opt["A"]));
+  console.log(chalk.hex("#dced6d")("B: " + opt.B));
+  console.log(chalk.hex("#dced6d")("C: " + opt.C));
+  console.log(chalk.hex("#dced6d")("D: " + opt.D));
+  console.log();
+  let userAnswer = readlineSync.question("Your answer: ");
 
-  if (userAnswer === answer) {
-    console.log(chalk.hex('#02f517')("RIGHT!!!!"))
-    score +=1
+  if (userAnswer.toUpperCase() === answer) {
+    console.log(chalk.hex("#02f517")("RIGHT!!!!"));
+    score += 1;
+  } else {
+    console.log(chalk.hex("#ff0020")("WRONG!!!"));
   }
-  else {
-    console.log(chalk.hex('#ff0020')('WRONG!!!'))
-  }
-  console.log()
-  
-  console.log("Your current score = " + score + "\n")
+  console.log();
+
+  console.log("Your current score = " + score + "\n");
 }
 
-for (let i = 0; i< questions.length; i = i+1) {
+for (let i = 0; i < questions.length; i = i + 1) {
   currentQuestion = questions[i];
-  checkTheAnswer(currentQuestion.question, currentQuestion.answer)
+  checkTheAnswer(
+    currentQuestion.question,
+    currentQuestion.options,
+    currentQuestion.answer
+  );
 }
 
-console.log(chalk.hex('#05fcf0')("------------------------------------"))
-console.log(chalk.hex('#05fcf0')("Your final score = " + score))
-console.log(chalk.hex('#05fcf0')("------------------------------------"))
-
+console.log(chalk.hex("#05fcf0")("------------------------------------"));
+console.log(chalk.hex("#05fcf0")("Your final score = " + score));
+console.log(chalk.hex("#05fcf0")("------------------------------------"));
