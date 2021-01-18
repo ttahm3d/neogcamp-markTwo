ab17b747f0b3989e75258a9f53b86b518bb9a761
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-01-18 10:21:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,119 @@
-var readlineSync = require("readline-sync");
-var chalk = require("chalk");
+var readlineSync = require('readline-sync')
+var chalk = require('chalk')
+chalk.bgRgb(15, 100, 204).inverse('Hello!')
 
-console.log("*********THE TOUGHEST GAME OF THRONES QUIZ*********");
-console.log();
+console.log("*********THE TOUGHEST GAME OF THRONES QUIZ*********")
+console.log()
 var userName = readlineSync.question(chalk.yellow.bold("What is your name? "));
-console.log(chalk.hex("#8e44ad").bold("Welcome " + userName + " !!\n"));
+console.log(chalk.hex('#8e44ad').bold("Welcome " + userName + " !!\n"))
+
+let highestScore = {
+  name: "Tahir",
+  score: 5
+}
+
+
+console.log(chalk.hex('#25ff55')("------------------------------------"))
+console.log(chalk.hex('#ffb800')("Highest Score till now"))
+console.log(chalk.hex('#ffb800')(highestScore.name))
+console.log(chalk.hex('#ffb800')(highestScore.score))
+console.log(chalk.hex('#25ff55')("------------------------------------"))
 
 var questions = [
   {
-    question:
-      "1. What were the exact words told by Ned Stark to Bran when he cold-bloodedly beheaded a deserter in the pilot episode? ",
-    options: {
-      A: "Our way is the old way",
-      B: "All men must die",
-      C: "Fear cuts deeper than a sword",
-      D: "It was my duty",
+    question: "1. What were the exact words told by Ned Stark to Bran when he cold-bloodedly beheaded a deserter in the pilot episode? ",
+    options:{
+      "A": "Our way is the old way",
+      "B": "All men must die",
+      "C": "Fear cuts deeper than a sword",
+      "D": "It was my duty"
     },
-    answer: "A",
+    answer: "A"
   },
   {
-    question:
-      "2. Where did Rhaegar Targaryen hid Lyanna Starka after abducting her? ",
-    options: {
-      A: "Volantis",
-      B: "Tyrosh",
-      C: "Dorne",
-      D: "Pentos",
+    question: "2. Where did Rhaegar Targaryen hid Lyanna Starka after abducting her? ",
+    options:{
+      "A": "Volantis",
+      "B": "Tyrosh",
+      "C": "Dorne",
+      "D": "Pentos"
     },
-    answer: "C",
+    answer: "C"
   },
   {
     question: "3. Where Daenerys went to bring back her dragons? ",
     options: {
-      A: "Meereen",
-      B: "The House of Undying",
-      C: "Dragonstone",
-      D: "Qohor",
+      "A": "Meereen",
+      "B": "The House of Undying",
+      "C": "Dragonstone",
+      "D": "Qohor"
     },
-    answer: "B",
+    answer:"B"
   },
   {
     question: "4. What was Hodor's real name? ",
     options: {
-      A: "Wess",
-      B: "Walder",
-      C: "Waif",
-      D: "Wylis",
+      "A": "Wess",
+      "B": "Walder",
+      "C": "Waif",
+      "D": "Wylis"
     },
-    answer: "D",
+    answer: "D"
   },
   {
-    question: '5. Who was the head of "Brotherhood Without Banners?" ',
+    question:"5. Who was the head of \"Brotherhood Without Banners?\" ",
     options: {
-      A: "Beric",
-      B: "Thoros",
-      C: "Riddle",
-      D: "Steve",
+      "A": "Beric",
+      "B": "Thoros",
+      "C": "Riddle",
+      "D": "Steve"
     },
-    answer: "A",
-  },
-];
+    answer: "A"
+  }
+]
 
 var score = 0;
 
-function checkTheAnswer(question, options, answer) {
+function checkTheAnswer(question, options, answer){
   let que = question;
   let opt = options;
-  console.log(chalk.bgHex("#0f64cc").inverse(que));
-  console.log();
-  console.log(chalk.hex("#dced6d")("A: " + opt["A"]));
-  console.log(chalk.hex("#dced6d")("B: " + opt.B));
-  console.log(chalk.hex("#dced6d")("C: " + opt.C));
-  console.log(chalk.hex("#dced6d")("D: " + opt.D));
-  console.log();
-  let userAnswer = readlineSync.question("Your answer: ");
-
+  console.log(chalk.bgHex("#0f64cc").inverse(que))
+  console.log()
+  console.log(chalk.hex('#dced6d')("A: " + opt["A"]))
+  console.log(chalk.hex('#dced6d')("B: " + opt.B))
+  console.log(chalk.hex('#dced6d')("C: " + opt.C))
+  console.log(chalk.hex('#dced6d')("D: " + opt.D))
+  console.log()
+  let userAnswer = readlineSync.question("Your answer: ")
+  
   if (userAnswer.toUpperCase() === answer) {
-    console.log(chalk.hex("#02f517")("RIGHT!!!!"));
-    score += 1;
-  } else {
-    console.log(chalk.hex("#ff0020")("WRONG!!!"));
+    console.log(chalk.hex('#02f517')("RIGHT!!!!"))
+    score +=1
   }
-  console.log();
-
-  console.log("Your current score = " + score + "\n");
+  else {
+    console.log(chalk.hex('#ff0020')('WRONG!!!'))
+  }
+  console.log()
+  
+  console.log("Your current score = " + score + "\n")
 }
 
-for (let i = 0; i < questions.length; i = i + 1) {
+for (let i = 0; i< questions.length; i = i+1) {
   currentQuestion = questions[i];
-  checkTheAnswer(
-    currentQuestion.question,
-    currentQuestion.options,
-    currentQuestion.answer
-  );
+  checkTheAnswer(currentQuestion.question, currentQuestion.options, currentQuestion.answer)
 }
 
-console.log(chalk.hex("#05fcf0")("------------------------------------"));
-console.log(chalk.hex("#05fcf0")("Your final score = " + score));
-console.log(chalk.hex("#05fcf0")("------------------------------------"));
+console.log(chalk.hex('#05fcf0')("------------------------------------"))
+console.log(chalk.hex('#05fcf0')("Your final score = " + score))
+console.log(chalk.hex('#05fcf0')("------------------------------------"))
+
+
+if(score >= highestScore.score) {
+  highestScore.name = userName;
+  
+  console.log(chalk.hex('#25ff55')("------------------------------------"))
+  console.log(chalk.hex('#ffb800')("You have scored the highest score so far"))
+  console.log(chalk.hex('#ffb800')(highestScore.name))
+  console.log(chalk.hex('#25ff55')("------------------------------------"))
+}
+
